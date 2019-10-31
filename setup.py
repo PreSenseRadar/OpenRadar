9a6065810bec305c4e331a5eac6ff4a962d0d10b
Edwin Pan <edwin.h.pan@gmail.com>
2019-10-31 01:54:09 -0500

    pipy process is ready for pushing

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def versionDev():
+    from setuptools_scm.version import get_local_dirty_tag
+    def clean_scheme(version):
+        return get_local_dirty_tag(version) if version.dirty else ''
+
+    return {'local_scheme': clean_scheme}
+
 setuptools.setup(
     name="openradar",
     version="1.0.0",
@@ -17,4 +24,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    use_scm_version = versionDev,
+    setup_requires=['setuptools_scm'],
 )
